c9ea70220f9263d5e54c96609fb88943ff6bd57b
xiabob <xiabob@yeah.net>
2017-08-10 17:17:56 +0800

    refine project directory

--- Class/WebColor.swift
+++ Class/WebColor.swift
@@ -1,13 +1,158 @@
 //
-//  String+UIColor.swift
+//  WebColor.swift
 //  WebColor
 //
-//  Created by xiabob on 16/12/13.
-//  Copyright © 2016年 xiabob. All rights reserved.
+//  Created by xiabob on 2017/8/10.
+//  Copyright © 2017年 xiabob. All rights reserved.
 //
 
 import Foundation
 import UIKit
+
+//MARK: - Web Colors Dic
+
+//https://en.wikipedia.org/wiki/Web_colors
+fileprivate let webColorsDic = [
+    "antiquewhite":          "FAEBD7" ,
+    "aqua":                  "00FFFF" ,
+    "aquamarine":            "7FFFD4" ,
+    "azure":                 "F0FFFF" ,
+    "beige":                 "F5F5DC" ,
+    "bisque":                "FFE4C4" ,
+    "black":                 "000000" ,
+    "blanchedalmond":        "FFEBCD" ,
+    "blue":                  "0000FF" ,
+    "blueviolet":            "8A2BE2" ,
+    "brown":                 "A52A2A" ,
+    "burlywood":             "DEB887" ,
+    "cadetblue":             "5F9EA0" ,
+    "chartreuse":            "7FFF00" ,
+    "chocolate":             "D2691E" ,
+    "coral":                 "FF7F50" ,
+    "cornflowerblue":        "6495ED" ,
+    "cornsilk":              "FFF8DC" ,
+    "crimson":               "DC143C" ,
+    "cyan":                  "00FFFF" ,
+    "darkblue":              "00008B" ,
+    "darkcyan":              "008B8B" ,
+    "darkgoldenrod":         "B8860B" ,
+    "darkgray":              "A9A9A9" ,
+    "darkgreen":             "006400" ,
+    "darkkhaki":             "BDB76B" ,
+    "darkmagenta":           "8B008B" ,
+    "darkolivegreen":        "556B2F" ,
+    "darkorange":            "FF8C00" ,
+    "darkorchid":            "9932CC" ,
+    "darkred":               "8B0000" ,
+    "darksalmon":            "E9967A" ,
+    "darkseagreen":          "8FBC8F" ,
+    "darkslateblue":         "483D8B" ,
+    "darkslategray":         "2F4F4F" ,
+    "darkturquoise":         "00CED1" ,
+    "darkviolet":            "9400D3" ,
+    "deeppink":              "FF1493" ,
+    "deepskyblue":           "00BFFF" ,
+    "dimgray":               "696969" ,
+    "dodgerblue":            "1E90FF" ,
+    "firebrick":             "B22222" ,
+    "floralwhite":           "FFFAF0" ,
+    "forestgreen":           "228B22" ,
+    "fuchsia":               "FF00FF" ,
+    "gainsboro":             "DCDCDC" ,
+    "ghostwhite":            "F8F8FF" ,
+    "gold":                  "FFD700" ,
+    "goldenrod":             "DAA520" ,
+    "gray":                  "808080" ,
+    "green":                 "008000" ,
+    "greenyellow":           "ADFF2F" ,
+    "honeydew":              "F0FFF0" ,
+    "hotpink":               "FF69B4" ,
+    "indianred":             "CD5C5C" ,
+    "indigo":                "4B0082" ,
+    "ivory":                 "FFFFF0" ,
+    "khaki":                 "F0E68C" ,
+    "lavender":              "E6E6FA" ,
+    "lavenderblush":         "FFF0F5" ,
+    "lawngreen":             "7CFC00" ,
+    "lemonchiffon":          "FFFACD" ,
+    "lightblue":             "ADD8E6" ,
+    "lightcoral":            "F08080" ,
+    "lightcyan":             "E0FFFF" ,
+    "lightgoldenrodyellow":  "FAFAD2" ,
+    "lightgreen":            "90EE90" ,
+    "lightgrey":             "D3D3D3" ,
+    "lightpink":             "FFB6C1" ,
+    "lightsalmon":           "FFA07A" ,
+    "lightseagreen":         "20B2AA" ,
+    "lightskyblue":          "87CEFA" ,
+    "lightslategray":        "778899" ,
+    "lightsteelblue":        "B0C4DE" ,
+    "lightyellow":           "FFFFE0" ,
+    "lime":                  "00FF00" ,
+    "limegreen":             "32CD32" ,
+    "linen":                 "FAF0E6" ,
+    "magenta":               "FF00FF" ,
+    "maroon":                "800000" ,
+    "mediumaquamarine":      "66CDAA" ,
+    "mediumblue":            "0000CD" ,
+    "mediumorchid":          "BA55D3" ,
+    "mediumpurple":          "9370DB" ,
+    "mediumseagreen":        "3CB371" ,
+    "mediumslateblue":       "7B68EE" ,
+    "mediumspringgreen":     "00FA9A" ,
+    "mediumturquoise":       "48D1CC" ,
+    "mediumvioletred":       "C71585" ,
+    "midnightblue":          "191970" ,
+    "mintcream":             "F5FFFA" ,
+    "mistyrose":             "FFE4E1" ,
+    "moccasin":              "FFE4B5" ,
+    "navajowhite":           "FFDEAD" ,
+    "navy":                  "000080" ,
+    "oldlace":               "FDF5E6" ,
+    "olive":                 "808000" ,
+    "olivedrab":             "6B8E23" ,
+    "orange":                "FFA500" ,
+    "orangered":             "FF4500" ,
+    "orchid":                "DA70D6" ,
+    "palegoldenrod":         "EEE8AA" ,
+    "palegreen":             "98FB98" ,
+    "paleturquoise":         "AFEEEE" ,
+    "palevioletred":         "DB7093" ,
+    "papayawhip":            "FFEFD5" ,
+    "peachpuff":             "FFDAB9" ,
+    "peru":                  "CD853F" ,
+    "pink":                  "FFC0CB" ,
+    "plum":                  "DDA0DD" ,
+    "powderblue":            "B0E0E6" ,
+    "purple":                "800080" ,
+    "red":                   "FF0000" ,
+    "rosybrown":             "BC8F8F" ,
+    "royalblue":             "4169E1" ,
+    "saddlebrown":           "8B4513" ,
+    "salmon":                "FA8072" ,
+    "sandybrown":            "F4A460" ,
+    "seagreen":              "2E8B57" ,
+    "seashell":              "FFF5EE" ,
+    "sienna":                "A0522D" ,
+    "silver":                "C0C0C0" ,
+    "skyblue":               "87CEEB" ,
+    "slateblue":             "6A5ACD" ,
+    "slategray":             "708090" ,
+    "snow":                  "FFFAFA" ,
+    "springgreen":           "00FF7F" ,
+    "steelblue":             "4682B4" ,
+    "tan":                   "D2B48C" ,
+    "teal":                  "008080" ,
+    "thistle":               "D8BFD8" ,
+    "tomato":                "FF6347" ,
+    "turquoise":             "40E0D0" ,
+    "violet":                "EE82EE" ,
+    "wheat":                 "F5DEB3" ,
+    "white":                 "FFFFFF" ,
+    "whitesmoke":            "F5F5F5" ,
+    "yellow":                "FFFF00" ,
+    "yellowgreen":           "9ACD32"
+]
 
 
 fileprivate let colorsCache = NSCache<AnyObject, UIColor>()
@@ -15,7 +160,7 @@ fileprivate let hexRegex = try? NSRegularExpression(pattern: "[0-9A-Fa-f]{3,8}",
 
 
 //MARK: - HEX
-public extension String {
+extension String {
     //#000
     func shortRgbHexCodeColor() -> UIColor? {
         var color = colorsCache.object(forKey: self as AnyObject)
@@ -143,7 +288,7 @@ public extension String {
 
 
 //MARK: - RGB
-public extension String {
+extension String {
     
     //rgb(255, 160, 122)
     func rgbCodeColor() -> UIColor? {
@@ -218,9 +363,9 @@ public extension String {
 }
 
 
-    
+
 //MARK: - HSL
-public extension String {
+extension String {
     //https://en.wikipedia.org/wiki/HSL_and_HSV
     
     //hsl(120, 100%, 50%)
@@ -292,7 +437,7 @@ public extension String {
 
 
 //MARK: - HSB/HSV
-public extension String {
+extension String {
     //https://en.wikipedia.org/wiki/HSL_and_HSV
     
     func hsbCodeColor() -> UIColor? {
@@ -369,9 +514,9 @@ public extension String {
     }
 }
 
-    
+
 //MARK: - Named
-public extension String {
+extension String {
     
     public func namedColor() -> UIColor? {
         //from cache
@@ -384,7 +529,7 @@ public extension String {
         let colorCode = webColorsDic[lowercased()]
         if colorCode != nil {color = colorCode!.rgbHexCodeColor()}
         if color != nil {return color}
-
+        
         //from self
         var sel = NSSelectorFromString(self)
         if UIColor.responds(to: sel) {
@@ -405,7 +550,7 @@ public extension String {
 }
 
 
-//MARK: - API
+//MARK: - API Methods
 public extension String {
     
     public func color() -> UIColor? {
@@ -482,3 +627,4 @@ extension UIColor {
         self.init(red: r1+m, green: g1+m, blue: b1+m, alpha: a)
     }
 }
+
